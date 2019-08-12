e8547df2a13d9a94f700f80d9081384324708843
rodrigohaddad <rgh@poli.ufrj.br>
2019-08-12 18:06:11 -0300

    Comment - 5

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ from projeto import square, root
 #    def test(self):
 #        self.assertEqual(square(3), 9)
 
-class My2Test(unittest.TestCase):
-    def test(self):
-        self.assertEqual(square(5), 25)
+#class My2Test(unittest.TestCase):
+#    def test(self):
+#        self.assertEqual(square(5), 25)
 
-class My3Test(unittest.TestCase):
-    def test(self):
-        self.assertEqual(square(7), 49)
+#class My3Test(unittest.TestCase):
+#    def test(self):
+#        self.assertEqual(square(7), 49)
 
 class My4Test(unittest.TestCase):
     def test(self):
