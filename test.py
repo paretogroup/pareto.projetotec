b1302423547222df1667f2dff360f7b8175c1d93
rodrigohaddad <rgh@poli.ufrj.br>
2019-08-12 17:59:22 -0300

    Comment - 3

--- test.py
+++ test.py
@@ -9,11 +9,11 @@ class My2Test(unittest.TestCase):
     def test(self):
         self.assertEqual(square(5), 25)
 
-#class My3Test(unittest.TestCase):
-#    def test(self):
-#        self.assertEqual(square(7), 49)
+class My3Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(square(7), 49)
 
-#class My4Test(unittest.TestCase):
-#    def test(self):
-#        self.assertEqual(root(9), 4)
+class My4Test(unittest.TestCase):
+    def test(self):
+        self.assertEqual(root(9), 4)
 
